c723a065b634df531846f6f9053a625739c4c1a2
Brian Q Anderson <bqanderson@me.com>
2018-11-12 16:50:09 -0600

    Add edit funtionality.

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -3,13 +3,28 @@ import { Consumer } from '../../context';
 import TextInputGroup from '../layout/TextInputGroup';
 import axios from 'axios';
 
-class AddContact extends Component {
+class EditContact extends Component {
   state = {
     name: '',
     email: '',
     phone: '',
     errors: {}
   };
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+
+    const contact = res.data;
+
+    this.setState({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone
+    });
+  }
 
   onSubmit = async (dispatch, e) => {
     e.preventDefault();
@@ -36,18 +51,16 @@ class AddContact extends Component {
       return;
     }
 
-    const newContact = {
-      name,
-      email,
-      phone
-    };
+    const updContact = { name, email, phone };
 
-    const res = await axios.post(
-      `https://jsonplaceholder.typicode.com/users`,
-      newContact
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
     );
 
-    dispatch({ type: 'ADD_CONTACT', payload: res.data });
+    dispatch({ type: 'UPDATE_CONTACT', payload: res.data });
 
     this.setState({
       name: '',
@@ -70,7 +83,7 @@ class AddContact extends Component {
           const { dispatch } = value;
           return (
             <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
+              <div className="card-header">Edit Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
@@ -100,7 +113,7 @@ class AddContact extends Component {
                   />
                   <input
                     type="submit"
-                    value="Add Contact"
+                    value="Update Contact"
                     className="btn btn-light btn-block"
                   />
                 </form>
@@ -113,4 +126,4 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+export default EditContact;
--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -16,6 +17,15 @@ const reducer = (state, action) => {
         ...state,
         contacts: [action.payload, ...state.contacts]
       };
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        )
+      };
     default:
       return state;
   }
@@ -23,28 +33,15 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '612-555-1234'
-      },
-      {
-        id: 2,
-        name: 'Jane Smith',
-        email: 'jsmith@hotmail.com',
-        phone: '612-555-5678'
-      },
-      {
-        id: 3,
-        name: 'Bob Johnson',
-        email: 'bjohnson@hotmail.com',
-        phone: '612-555-90123'
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    this.setState({ contacts: res.data });
+  }
 
   render() {
     return (
